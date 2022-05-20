dc5c1bd520f7d1d3973dd24893449987a19f8819
Juanrico Alvaro <juanricoalvaro@gmail.com>
2022-05-21 00:37:47 +0800

    second commit

---
+++ my_tutor/lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:my_tutor/views/landingForm.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MainSplashScreen(),
+    );
+  }
+}
+
+class MainSplashScreen extends StatefulWidget {
+  const MainSplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainSplashScreen> createState() => _MainSplashScreenState();
+}
+
+class _MainSplashScreenState extends State<MainSplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 5), () {
+      setState(() {});
+      // Navigator.of(context).push(LandingFormRoute());
+      Navigator.push(context, FadeRoute(page: const LandingForm()));
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                Color.fromARGB(255, 72, 212, 251),
+                Color.fromARGB(255, 16, 133, 228)
+              ])),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(height: 200),
+              Text('My Tutor',
+                  style: GoogleFonts.arizonia(
+                      textStyle: const TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ))),
+              SizedBox(
+                height: 200,
+                child: Image.asset(
+                  'assets/logo.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(height: 150),
+              const LinearProgressIndicator(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FadeRoute extends PageRouteBuilder {
+  final Widget page;
+  FadeRoute({required this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
+        );
+}
---
+++ my_tutor/lib/views/landingForm.dart
@@ -0,0 +1,120 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_tutor/views/registerForm.dart';
+
+class LandingForm extends StatefulWidget {
+  const LandingForm({Key? key}) : super(key: key);
+
+  @override
+  State<LandingForm> createState() => _LandingFormState();
+}
+
+class _LandingFormState extends State<LandingForm> {
+  Widget _submitButton() {
+    return InkWell(
+      onTap: () {
+        // Navigator.push(
+        //     context, MaterialPageRoute(builder: (context) => LoginPage()));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Colors.white),
+        child: const Text(
+          'Login',
+          style:
+              TextStyle(fontSize: 20, color: Color.fromARGB(255, 16, 133, 228)),
+        ),
+      ),
+    );
+  }
+
+  Widget _signUpButton() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => RegisterForm()));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          border: Border.all(color: Colors.white, width: 2),
+        ),
+        child: const Text(
+          'Register now',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _title() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('My Tutor',
+              style: GoogleFonts.arizonia(
+                  textStyle: const TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ))),
+          SizedBox(
+            height: 200,
+            child: Image.asset(
+              'assets/logo.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(255, 72, 212, 251),
+                    Color.fromARGB(255, 16, 133, 228)
+                  ])),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _title(),
+              const SizedBox(
+                height: 30,
+              ),
+              _submitButton(),
+              const SizedBox(
+                height: 20,
+              ),
+              _signUpButton(),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
